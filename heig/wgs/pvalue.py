fix pvalue computation issue

heig/wgs/pvalue.py
@@ -32,8 +32,8 @@ def saddle(score_stat, egvalues, wcov_mat):
     # normalize eigenvalues
     n_voxels = len(score_stat)
     max_egvalue = egvalues[0]
-    score_stat /= max_egvalue
-    egvalues /= max_egvalue
+    score_stat = score_stat / max_egvalue
+    egvalues = egvalues / max_egvalue
 
     xmin = -len(egvalues) / (2 * score_stat)
     xmin[score_stat > np.sum(egvalues)] = -0.01
@@ -141,9 +141,11 @@ def _handle_invalid_pvalues(score_stat, wcov_mat):
 
     """
     c1 = np.sum(np.diag(wcov_mat))
-    wcov_mat = np.dot(wcov_mat, wcov_mat)
+    # wcov_mat = np.dot(wcov_mat, wcov_mat)
+    wcov_mat = wcov_mat ** 2
     c2 = np.sum(np.diag(wcov_mat))
-    wcov_mat = np.dot(wcov_mat, wcov_mat)
+    # wcov_mat = np.dot(wcov_mat, wcov_mat)
+    wcov_mat = wcov_mat ** 2
     c4 = np.sum(np.diag(wcov_mat))
 
     score_stat = (score_stat - c1) / np.sqrt(2 * c2)
@@ -234,8 +236,8 @@ def saddle2(score_stat, egvalues):
         egvalues = egvalues.reshape(-1, 1)
     score_stat[score_stat <= 0] = 0.0001
     d = np.max(egvalues)
-    egvalues /= d
-    score_stat /= d
+    egvalues = egvalues / d
+    score_stat = score_stat / d
     n = len(egvalues)
     n_voxels = len(score_stat)
 
@@ -243,17 +245,16 @@ def saddle2(score_stat, egvalues):
     xmin[score_stat > np.sum(egvalues)] = -0.01
     xmax = np.ones(n_voxels) * 1 / egvalues[0] * 0.499995
 
-    xhat = _bisection(egvalues, score_stat, xmin, xmax)
-    w = np.sqrt(2 * (xhat * score_stat - _k(xhat, egvalues)))
-    w[xhat < 0] *= -1
-    w[xhat == 0] = 0
-    v = xhat * np.sqrt(_k2(xhat, egvalues))
-
     res = np.zeros(n_voxels)
+    xhat = _bisection(egvalues, score_stat, xmin, xmax)
     valid_res = abs(xhat) >= 0.0001
-    res[valid_res] = norm.sf(
-        w[valid_res] + np.log(v[valid_res] / w[valid_res]) / w[valid_res], 0, 1
-    )
+    xhat_valid = xhat[valid_res]
+
+    w = np.sqrt(2 * (xhat_valid * score_stat[valid_res] - _k(xhat_valid, egvalues)))
+    w[xhat_valid < 0] *= -1
+    w[xhat_valid == 0] = 0
+    v = xhat_valid * np.sqrt(_k2(xhat_valid, egvalues))
+    res[valid_res] = norm.sf(w + np.log(v / w) / w, 0, 1)
 
     if (~valid_res).any():
         res[~valid_res] = liu_mod(score_stat[~valid_res], egvalues)

test/test_wgs/test_pvalue.py
@@ -10,34 +10,42 @@ from heig.wgs.pvalue import saddle, _handle_invalid_pvalues, liu, liu_mod, saddl
 class Test_saddle(unittest.TestCase):
     def test_saddle(self):
         ## single voxel
-        pvalues = saddle(np.array([6.0]), np.array([4.0, 3.0, 2.0, 1.0]), None)
+        pvalues = saddle2(np.array([6.0]), np.array([4.0, 3.0, 2.0, 1.0]))
         assert_array_almost_equal(pvalues, np.array([0.6329193]))
 
         ## multiple voxels
-        pvalues = saddle(np.array([6.0, 5.0]), np.array([4.0, 3.0, 2.0, 1.0]), None)
+        pvalues = saddle2(np.array([6.0, 5.0]), np.array([4.0, 3.0, 2.0, 1.0]))
         assert_array_almost_equal(pvalues, np.array([0.6329193, 0.7069172]))
 
         ## extreme voxels
-        pvalues = saddle(np.array([60.0]), np.array([4.0, 3.0, 2.0, 1.0]), None)
+        pvalues = saddle2(np.array([60.0]), np.array([4.0, 3.0, 2.0, 1.0]))
         assert_array_almost_equal(pvalues, np.array([0.0004239342]))
 
-        pvalues = saddle(np.array([600.0]), np.array([4.0, 3.0, 2.0, 1.0]), None)
+        pvalues = saddle2(np.array([600.0]), np.array([4.0, 3.0, 2.0, 1.0]))
         assert_array_almost_equal(pvalues, np.array([6.531105e-34]))
 
         ## very small stats
-        pvalues = saddle(np.array([0.1]), np.array([4.0, 3.0, 2.0, 1.0]), None)
+        pvalues = saddle2(np.array([0.1]), np.array([4.0, 3.0, 2.0, 1.0]))
         assert_array_almost_equal(pvalues, np.array([0.9997457]))
 
-        pvalues = saddle(np.array([0.0001]), np.array([4.0, 3.0, 2.0, 1.0]), None)
+        pvalues = saddle2(np.array([0.0001]), np.array([4.0, 3.0, 2.0, 1.0]))
         assert_array_almost_equal(pvalues, np.array([1.0]))
+        
+        ## corner cases
+        pvalues = saddle2(np.array([6.0]), np.array([3.0, 2.0, 1.0]))
+        assert_array_almost_equal(pvalues, np.array([0.36787944]))
+
+        pvalues = saddle2(np.array([10.0]), np.array([4.0, 3.0, 2.0, 1.0]))
+        assert_array_almost_equal(pvalues, np.array([0.38244247]))
+
 
     def test_handle_invalid_pvalues(self):
         wcov_mat = np.arange(12).reshape(4, 3) / 50
         wcov_mat = np.dot(wcov_mat.T, wcov_mat)
         egvalues, _ = np.linalg.eigh(wcov_mat)
         egvalues = np.flip(egvalues)
-        pvalues1 = _handle_invalid_pvalues(np.array([60.0, 6.0]), wcov_mat)
-        pvalues2 = saddle(np.array([60.0, 6.0]), egvalues, None)
+        pvalues1 = liu_mod(np.array([60.0, 6.0]), egvalues)
+        pvalues2 = saddle2(np.array([60.0, 6.0]), egvalues)
         assert_array_almost_equal(pvalues1, pvalues2)
 
 
@@ -61,7 +69,10 @@ if __name__ == "__main__":
     egvalues = np.array([1, 0.63457944])
     print(saddle(score_stats, egvalues, 
                  np.array([[1.48967545e-02, 1.05388240e-06],
-                          [1.05388240e-06, 9.45317448e-03]])))
+                          [1.05388240e-06, 9.45317448e-03]]) * 67.12873613372044))
     print(saddle2(score_stats, egvalues))
     print(liu(score_stats, egvalues))
     print(liu_mod(score_stats, egvalues))
+
+    print(saddle2(np.array([6.0]), np.array([3.0, 2.0, 1.0])))
+    print(saddle2(np.array([10.0]), np.array([4.0, 3.0, 2.0, 1.0])))